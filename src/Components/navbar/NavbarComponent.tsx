05722a2a0572feac7943d01f95452505d0ef22ce
Carlos Aguilar <carlosaguilarpaniagua@gmail.com>
2024-05-30 21:42:09 -0600

    Add navbar component

---
+++ src/Components/navbar/NavbarComponent.tsx
@@ -0,0 +1,29 @@
+import { Component, useState } from "react";
+import "../navbar/navbar.css";
+import { Navbar, Nav } from 'react-bootstrap';
+
+export const NavbarComponent =()=> {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">
+        <img
+          src="/logo.png" // Update with your logo path
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="Logo"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#home" className="nav-link-active">Home</Nav.Link>
+          <Nav.Link href="#shop">Shop</Nav.Link>
+          <Nav.Link href="#blog">Blog</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
